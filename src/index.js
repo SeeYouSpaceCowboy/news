3bf71163ef52885b2a33355fbf720aa9c5c54884
Mohammed Chisti <93mchisti@gmail.com>
2018-04-14 02:53:08 -0400

    Added redux folders.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import App from './components/App'
+import reducers from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
